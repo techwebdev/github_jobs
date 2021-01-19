95c5a37c011b45b41c8d5ef783a31cd5092a7d64
techwebdev <amit.kadivar3@gmail.com>
2021-01-19 20:06:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
+import JobsPagination from "./JobsPagination";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1>Loading ..</h1>}
+      {console.log(error)}
+      {error && (
+        <h1>
+          Error. Try Refreshing page.<sub>{error}</sub>
+        </h1>
+      )}
+      {jobs &&
+        jobs.map((job) => {
+          return <Job key={job.id} job={job}></Job>;
+        })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+    </Container>
   );
 }
 
