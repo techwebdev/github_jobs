8f21ede2a3aa5620364250699490cc9edbad4926
techwebdev <amit.kadivar3@gmail.com>
2021-01-31 23:42:15 +0530

    work on details page

---
+++ src/JobDetails.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Card, Badge, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import ReactMarkdown from "react-markdown";
+import { useFetchSingleJobs } from "./useFetchJobs";
+
+function JobDetails({ match }) {
+  console.log({ id: match.params.id });
+  const { job, loading, error } = useFetchSingleJobs(match.params.id);
+  console.log({ job });
+  return (
+    <>
+      <Card className="mb-3">
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>Error. Try Refreshing.</h1>}
+        {job && !loading && (
+          <Card.Body>
+            <div>
+              <Link to="/">
+                <Button variant="primary">Back to Home</Button>
+              </Link>
+            </div>
+            <div className="d-flex justify-content-between">
+              <div>
+                <Card.Title>
+                  {job.title} -{" "}
+                  <span className="text-muted font-weight-light">
+                    {job.company}
+                  </span>
+                </Card.Title>
+                <Card.Subtitle className="text-muted mb-2">
+                  {job &&
+                    job.created_at &&
+                    new Date(job.created_at).toLocaleDateString()}
+                </Card.Subtitle>
+                <Badge variant="secondary" className="mr-2">
+                  {job.type}
+                </Badge>
+                <Badge variant="secondary">{job.location}</Badge>
+              </div>
+              <img
+                className="d-none d-md-block"
+                height="50"
+                alt={job.company}
+                src={job.company_logo}
+              />
+            </div>
+            <Card.Text>
+              <div className="mt-4">
+                <ReactMarkdown source={job.description} />
+              </div>
+              <div style={{ wordBreak: "break-all" }}>
+                <ReactMarkdown source={job.how_to_apply} />
+              </div>
+            </Card.Text>
+          </Card.Body>
+        )}
+      </Card>
+    </>
+  );
+}
+
+export default JobDetails;
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,62 +1,112 @@
-import { useReducer, useEffect } from 'react'
-import axios from 'axios'
+import { useReducer, useEffect } from "react";
+import axios from "axios";
 
 const ACTIONS = {
-  MAKE_REQUEST: 'make-request',
-  GET_DATA: 'get-data',
-  ERROR: 'error',
-  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
-}
-
-const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
-
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+  UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
+  MAKE_SINGLE_REQUEST: "nake-single-request",
+  SINGLE_DATA: "single-data",
+};
+const BASE_URL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/";
+const GET_JOBS = `${BASE_URL}positions.json`;
+const SINGLE_URL = `${BASE_URL}positions/`;
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
-      return { loading: true, jobs: [] }
+      return { loading: true, jobs: [] };
     case ACTIONS.GET_DATA:
-      return { ...state, loading: false, jobs: action.payload.jobs }
+      return { ...state, loading: false, jobs: action.payload.jobs };
     case ACTIONS.ERROR:
-      return { ...state, loading: false, error: action.payload.error, jobs: [] }
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
     case ACTIONS.UPDATE_HAS_NEXT_PAGE:
-      return { ...state, hasNextPage: action.payload.hasNextPage }
+      return { ...state, hasNextPage: action.payload.hasNextPage };
+
+    case ACTIONS.MAKE_SINGLE_REQUEST:
+      return { ...state, loading: true, job: {} };
+
+    case ACTIONS.SINGLE_DATA:
+      return { ...state, loading: false, job: action.payload.job };
     default:
-      return state
+      return state;
   }
 }
 
 export default function useFetchJobs(params, page) {
-  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   useEffect(() => {
-    const cancelToken1 = axios.CancelToken.source()
-    dispatch({ type: ACTIONS.MAKE_REQUEST })
-    axios.get(BASE_URL, {
-      cancelToken: cancelToken1.token,
-      params: { markdown: true, page: page, ...params }
-    }).then(res => {
-      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } }) 
-    }).catch(e => {
-      if (axios.isCancel(e)) return
-      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
-    })
+    const cancelToken1 = axios.CancelToken.source();
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(GET_JOBS, {
+        cancelToken: cancelToken1.token,
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
 
-    const cancelToken2 = axios.CancelToken.source()
-    axios.get(BASE_URL, {
-      cancelToken: cancelToken2.token,
-      params: { markdown: true, page: page + 1, ...params }
-    }).then(res => {
-      dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } }) 
-    }).catch(e => {
-      if (axios.isCancel(e)) return
-      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
-    })
+    const cancelToken2 = axios.CancelToken.source();
+    axios
+      .get(GET_JOBS, {
+        cancelToken: cancelToken2.token,
+        params: { markdown: true, page: page + 1, ...params },
+      })
+      .then((res) => {
+        dispatch({
+          type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
+          payload: { hasNextPage: res.data.length !== 0 },
+        });
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
 
     return () => {
-      cancelToken1.cancel()
-      cancelToken2.cancel()
-    }
-  }, [params, page])
-  
-  return state
+      cancelToken1.cancel();
+      cancelToken2.cancel();
+    };
+  }, [params, page]);
+
+  return state;
+}
+
+export function useFetchSingleJobs(id) {
+  const [state, dispatch] = useReducer(reducer, { job: [], loading: true });
+
+  useEffect(() => {
+    const cancelToken1 = axios.CancelToken.source();
+    dispatch({ type: ACTIONS.MAKE_SINGLE_REQUEST });
+    axios
+      .get(`${SINGLE_URL}${id}.json`, {
+        cancelToken: cancelToken1.token,
+        params: { markdown: true },
+      })
+      .then((res) => {
+        console.log({ data: res.data });
+        dispatch({ type: ACTIONS.SINGLE_DATA, payload: { job: res.data } });
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+
+    return () => {
+      cancelToken1.cancel();
+    };
+  }, [id]);
+
+  return state;
 }
